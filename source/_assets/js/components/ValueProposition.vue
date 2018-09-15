0ab3e27da11c415e3123ade01bc2cdf2f0d99eff
AlexVipond <alex.r.vipond@gmail.com>
2018-09-15 14:49:40 -0600

    start lang support, force github to show production folder

--- source/_assets/js/components/ValueProposition.vue
+++ source/_assets/js/components/ValueProposition.vue
@@ -1,26 +1,45 @@
 <template>
   <div class="">
-    <div class="font-bold text-xl md:text-2xl text-grey-lightest text-shadow">
-      <h3 class="mb-2">Analog.</h3>
-      <h3>Digital.</h3>
+    <div class="font-bold text-xl md:text-2xl text-blue-lighter text-shadow">
+      <h3 class="mb-2">{{ [language].secondary_value }}.</h3>
+      <h3>{{ [language].tertiary_value }}.</h3>
     </div>
 
-    <h2 class="mt-4 mb-12 font-bold text-5xl md:text-6xl leading-none text-white text-shadow">Human.</h2>
+    <h2 class="mt-4 mb-12 font-bold text-5xl md:text-6xl leading-none text-blue-lightest text-shadow">{{ [language].primary_value }}.</h2>
 
-    <p class="mb-12 text-xl md:text-2xl leading-normal text-grey-lightest text-shadow">
-      Discover the power of a
-      <span class="font-medium text-white">human mastering engineer</span>.
+    <p class="mb-12 text-xl md:text-2xl leading-normal text-blue-lighter text-shadow">
+      We provide the
+      <span class="font-bold text-blue-lightest">finishing touch</span>
       <br class="hidden sm:block">
-      Get your music
-      <span class="font-medium text-white">louder</span>, <span class="font-medium text-white">polished</span>, and <span class="font-medium text-white">ready for distribution.</span>
+      to make your
+      <span class="font-bold text-blue-lightest">house</span>
+      feel like a
+      <span class="font-bold text-blue-lightest">home</span>.
     </p>
   </div>
 </template>
 
 <script>
 export default {
+  props: ['language'],
   data () {
-    return {}
+    return {
+      en: {
+        primary_value: 'Finish Carpentry',
+        secondary_value: 'Design',
+        tertiary_value: 'Installation'
+      },
+      es: {
+        primary_value: 'Finish Carpentry',
+        secondary_value: 'Design',
+        tertiary_value: 'Installation'
+      },
+      pt: {
+        primary_value: 'Finish Carpentry',
+        secondary_value: 'Design',
+        tertiary_value: 'Installation'
+      }
+    }
   }
 }
 </script>
